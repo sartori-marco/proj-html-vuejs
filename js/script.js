5dcc5190e02c740ff9ab08a193f17cbef4f04639
marco sartori <marcosartori98@yahoo.com>
2021-02-16 16:30:58 +0100

    culinary

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var app = new Vue({
   el: '#root',
   data: {
-    elements: ['Home', 'Recipes', 'Places', 'Blog', 'About', 'Contact']
+    elements: ['Home', 'Recipes', 'Places', 'Blog', 'About', 'Contact'],
+    menu: ['Recipes', 'Places', 'Blog', 'About', 'Contact'
+
+    ]
   }
 });
